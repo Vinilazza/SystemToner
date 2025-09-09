14961831e3d9a5ae1c64aeeecf684e05ab0d3831
Vinicius Lazarotto <vinilazza2016cris@gmail.com>
2025-09-09 15:46:29 -0300

    test

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,5 @@
 // api/index.js
-const app = require("../server"); // <- use esta se app.js está em backend/
+const app = require("../app"); // <- use esta se app.js está em backend/
 //// const app = require("../app");     // <- use esta se app.js está na raiz
 
 module.exports = app; // exporta o app para o runtime serverless do Vercel
